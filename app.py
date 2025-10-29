51aa17fb6a88d3b865c938b6eebcd6bcea8f25a3
DukeMinty <aarontrebusbusiness@protonmail.com>
2025-10-29 09:29:43 -0500

    Changed minor formatting of app.py to reduce redundancy

--- app.py
+++ app.py
@@ -1,14 +1,3 @@
-# from flask import Flask, render_template
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def home():
-#     return render_template("index.html")
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
 from datetime import datetime
 import requests
 import pygal
