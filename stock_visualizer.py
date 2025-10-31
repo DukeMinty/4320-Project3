5600dbb3b5e7447fb27b639ff3ea39c3648c25ef
Destiny Watson <destinywatson06@gmail.com>
2025-10-30 20:50:12 -0500

    Delete stock_visualizer.py

--- stock_visualizer.py
+++
@@ -1,7 +0,0 @@
-# ---------- End Screen Loop ----------
-while True:
-    main()
-    again = input("\nWould you like to search another stock? (y/n): ").lower()
-    if again != 'y':
-        print("\nThanks for using Stock Data Visualizer! Goodbye!")
-        break
