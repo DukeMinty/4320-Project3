d25f9bfe1c165ba06126904c905bcb7af8f5dd7e
Destiny Watson <destinywatson06@gmail.com>
2025-10-30 20:52:01 -0500

    Added end screen loop

--- app.py
+++ app.py
@@ -87,3 +87,12 @@ if(timeSeries == "2"):
     data = r.json()
 
 print(data)
+
+# ---------- End Screen Loop ----------
+while True:
+    main()
+    again = input("\nWould you like to search another stock? (y/n): ").lower()
+    if again != 'y':
+        print("\nThanks for using Stock Data Visualizer! Goodbye!")
+        break
+		
